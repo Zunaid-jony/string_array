bfa34b567bdb35dbaa96d38ccb5c58dd3df8e129
Zunaid-jony <jony35666666@gmail.com>
2022-04-03 14:06:15 +0600

    last update

---
+++ array-sort.js
@@ -0,0 +1,9 @@
+const number = [ 8,2,9,6,2,1,20];
+const sortNumber = number.sort();
+console.log(sortNumber);
+const friends = ['jamil','prionto','masud','aub','siplo'];
+const sortFriend = friends.sort();
+console.log(sortFriend);
+const sortFriends = friends.reverse();
+
+console.log(sortFriends);
